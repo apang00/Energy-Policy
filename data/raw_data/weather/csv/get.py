03a21966f403ffc1269b12a9399dc69c0f5423b9
pixel-echo <tara.chakkithara@icloud.com>
2024-02-14 13:11:21 -0500

    extracted temperature data from raw weather data with clean_temperature.R

---
+++ data/raw_data/weather/csv/get.py
@@ -0,0 +1,9 @@
+# import requests
+# url_city= "https://www.data.jma.go.jp/gmd/risk/obsdl/top/station"
+# payload = {   
+#     'pd': '11'
+# }
+# citylist = requests.post(url_city,data = payload)
+# print(citylist.text)
+
+import pandas as pd
---
+++ scripts/data_cleaning/clean_temperature.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(readxl)
+
+clean_csv <- function(file) {
+  data <- read_csv(sprintf("data/raw_data/weather/csv/%s_201.csv", file), skip = 6)
+  data <- data |>
+    select(1, 2, 3, 4) |>
+    rename(
+      year = names(data)[1],
+      month = names(data)[2],
+      day = names(data)[3],
+      temperature = names(data)[4]
+    )|>
+    filter(year >= 2005)
+  
+  return(data)
+}
+
+station_data <- read_excel("data/raw_data/weather/station.xlsx")
+
+total_data <- data.frame()
+count <- 1
+
+for (id in station_data$station_id) {
+  data <- clean_csv(id)
+  data <- data |>
+    mutate(
+      id = station_data$pref_id[count]
+    )
+  total_data <- rbind(total_data, data)
+  count <- count + 1
+}
+
+total_data <- total_data |>
+  group_by(id, year, month, day) |>
+  summarise(temperature_avg = mean(temperature, na.rm = TRUE)) |>
+  ungroup()
+
+
+write_csv(total_data, "data/analysis_data/clean_temperature.csv")
