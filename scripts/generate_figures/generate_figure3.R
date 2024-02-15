064792487ca35e4cb56461021ed71c9a5de125ed
pixel-echo <tara.chakkithara@icloud.com>
2024-02-15 02:34:11 -0500

    first draft

--- scripts/generate_figures/generate_figure3.R
+++ scripts/generate_figures/generate_figure3.R
@@ -12,13 +12,17 @@ data <- data |>
   group_by(year >= 2010) |>
   summarise(avg_rate = mean(dr)*100) |>
   ungroup() |>
-  rename(policy = "year >= 2010")
+  rename(policy = "year >= 2010") |>
+  mutate(policy = case_when(
+    policy == TRUE ~ "Post-Policy",
+    policy == FALSE ~ "Initial")
+  )
 
 plot <- data |>
   ggplot(aes(x = policy)) +
   theme_fivethirtyeight() +
   theme_bw() +
   geom_point(aes(y = avg_rate)) +
-  labs(x = "Policy", y = "Mortality Rate (%)")
+  labs(y = "Mortality", x = "")
 
 ggsave("other/figures/figure3.png", plot)
